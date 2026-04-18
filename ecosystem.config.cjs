3e6a6668bb2f8374a55ee40f53a4adbf83333e41
gintaras741 <gintaras.gaucys1@gmail.com>
2026-04-18 16:36:37 +0300

    change port

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -7,7 +7,7 @@ module.exports = {
             args: "start",
             env: {
                 NODE_ENV: "production",
-                PORT: 3000,
+                PORT: 4000,
                 CORS_ORIGINS: "https://an.gintaras.me,http://an.gintaras.me,http://localhost:5173,http://localhost:3000",
             },
         },
